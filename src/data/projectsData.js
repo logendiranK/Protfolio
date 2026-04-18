0a5bc16cd91d52ac10b892ca36b8185f712da37d
Logendiran K <logendiran2005@gmail.com>
2026-04-18 14:38:30 +0530

    updated content

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,15 +1,6 @@
 export const projectsData = [
   {
     id: 1,
-    title: "Rubiks Cubes",
-    description: "An interactive 3D Rubik’s Cube built using Three.js with smooth animations and user-controlled rotations.",
-    technologies: ["Three.js", "JavaScript", "HTML", "CSS"],
-    githubUrl: "https://github.com/logendiranK/threejs-rubiks-cube",
-    liveUrl: "https://cubes-0805.netlify.app/",
-    image: "https://github.com/logendiranK/threejs-rubiks-cube/blob/main/assets/rubiks-cube.png?raw=true"
-  },
-  {
-    id: 2,
     title: "AVLearn",
     description: "A gamified learning platform that makes education interactive through quizzes, progress tracking, and engaging game-based features.",
     technologies: ["React", "Node.js", "MongoDB"],
@@ -18,13 +9,21 @@ export const projectsData = [
     image: "https://via.placeholder.com/400x250?text=Project+2"
   },
   {
+    id: 2,
+    title: "Grocery Mart",
+    description: "A Grocery Mart website that allows you to view for products and add them to the cart.”",
+    technologies: ["React", "Node.js", "MongoDB"],
+    githubUrl: "https://github.com/logendiranK/Grocery-Mart-frontend",
+    liveUrl: "https://grocery-mart-frontend.vercel.app/",
+    image: "https://github.com/logendiranK/Gallery-Frontend/blob/main/assets/gallery.png?raw=true"
+  },
+  {
     id: 3,
-    title: "Temperature Converter",
-    description: "A simple and responsive temperature converter that instantly converts values between Celsius and Fahrenheit.”",
-    technologies: ["JavaScript", "HTML", "CSS"],
-    githubUrl: "https://github.com/logendiranK/intern-task-3-temperature-conveter",
-    liveUrl: "https://playful-palmier-d61319.netlify.app/",
-    image: "https://github.com/logendiranK/intern-task-3-temperature-conveter/blob/main/assets/temperature-converter.png?raw=true"
+    title: "Cloud-burst Early Warning And Alarm System",
+    description: "A comprehensive early warning and alarm system designed to detect and alert users about potential cloudburst events, ensuring timely evacuation and safety measures.",
+    technologies: ["IOT","Python"],
+    githubUrl: "https://github.com/logendiranK/Cloudburst-Early-Warning-System",
+    image: "https://github.com/logendiranK/Cloudburst-Early-Warning-System/blob/main/assets/cloud-burst.png?raw=true"
   },
   {
     id: 4,
@@ -46,15 +45,6 @@ export const projectsData = [
   },
   {
     id: 6,
-    title: "Grocery Mart",
-    description: "A Grocery Mart website that allows you to view for products and add them to the cart.”",
-    technologies: ["React", "Node.js", "MongoDB"],
-    githubUrl: "https://github.com/logendiranK/Grocery-Mart-frontend",
-    liveUrl: "https://grocery-mart-frontend.vercel.app/",
-    image: "https://github.com/logendiranK/Gallery-Frontend/blob/main/assets/gallery.png?raw=true"
-  },
-  {
-    id: 7,
     title: "Shift-X",
     description: "Shift-X is a dynamic and responsive website for a shift-based work management system, designed to streamline scheduling and team coordination.",
     technologies: ["React", "Node.js", "MongoDB"],
@@ -63,27 +53,37 @@ export const projectsData = [
     image: "https://github.com/logendiranK/SHIFT-X/blob/main/assets/shift-x.png?raw=true"
   },
   {
-    id: 8,
+    id: 7,
     title: "Chatbot",
     description: "A chatbot that can answer questions about the user needs.",
     technologies: ["React", "Node.js", "MongoDB","Ollama"],
     githubUrl: "https://github.com/logendiranK/Chatbot-frontend",
     image: "https://github.com/logendiranK/SHIFT-X/blob/main/assets/shift-x.png?raw=true"
-  },  
+  },    
   {
-    id: 9,
-    title: "Cloud-burst Early Warning And Alarm System",
-    description: "A comprehensive early warning and alarm system designed to detect and alert users about potential cloudburst events, ensuring timely evacuation and safety measures.",
-    technologies: ["IOT","Python"],
-    githubUrl: "https://github.com/logendiranK/Cloudburst-Early-Warning-System",
-    image: "https://github.com/logendiranK/Cloudburst-Early-Warning-System/blob/main/assets/cloud-burst.png?raw=true"
-  },  
-  {
-    id: 10,
+    id: 8,
     title: "Gallery",
     description: "A Gallery website that allows you to view for images.",
     technologies: ["React", "Node.js", "MongoDB"],
     githubUrl: "https://github.com/logendiranK/Gallery-Frontend",
     image: "https://github.com/logendiranK/Gallery-Frontend/blob/main/assets/gallery.png?raw=true"
   },  
+  {
+    id: 9,
+    title: "Rubiks Cubes",
+    description: "An interactive 3D Rubik’s Cube built using Three.js with smooth animations and user-controlled rotations.",
+    technologies: ["Three.js", "JavaScript", "HTML", "CSS"],
+    githubUrl: "https://github.com/logendiranK/threejs-rubiks-cube",
+    liveUrl: "https://cubes-0805.netlify.app/",
+    image: "https://github.com/logendiranK/threejs-rubiks-cube/blob/main/assets/rubiks-cube.png?raw=true"
+  },
+  {
+    id: 10,
+    title: "Temperature Converter",
+    description: "A simple and responsive temperature converter that instantly converts values between Celsius and Fahrenheit.”",
+    technologies: ["JavaScript", "HTML", "CSS"],
+    githubUrl: "https://github.com/logendiranK/intern-task-3-temperature-conveter",
+    liveUrl: "https://playful-palmier-d61319.netlify.app/",
+    image: "https://github.com/logendiranK/intern-task-3-temperature-conveter/blob/main/assets/temperature-converter.png?raw=true"
+  },
 ];
